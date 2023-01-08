b7c1e6bc414cc95f53aa57aaed7fee24f406cc82
Conor McManus <cnrmcmanus@gmail.com>
2023-01-08 22:55:54 +0000

    feat(ui): show line numbers in editor

--- src/ui/editor.rs
+++ src/ui/editor.rs
@@ -14,25 +14,28 @@ pub fn render<B: Backend>(frame: &mut Frame<B>, area: Rect, program: &Program) {
     let code_style = Style::default();
     let comment_style = Style::default().fg(Color::Rgb(150, 150, 150));
 
+    let line_count = program.editor.lines.len();
+    let line_count_digits = (line_count.checked_ilog10().unwrap_or(0) + 1) as usize;
     let text = program
         .editor
         .lines
         .iter()
         .enumerate()
         .map(|(i, line)| {
-            let spans = line
-                .chars()
-                .enumerate()
-                .map(|(j, chr)| {
-                    if program.cursor() == Some((i, j)) {
-                        Span::styled(chr.to_string(), focused_code_style)
-                    } else if program.instruction_positions.contains(&(i, j)) {
-                        Span::styled(chr.to_string(), code_style)
-                    } else {
-                        Span::styled(chr.to_string(), comment_style)
-                    }
-                })
-                .collect::<Vec<_>>();
+            let spans = std::iter::once(Span::styled(
+                format!("{:0>line_count_digits$} ", i + 1),
+                comment_style,
+            ))
+            .chain(line.chars().enumerate().map(|(j, chr)| {
+                if program.cursor() == Some((i, j)) {
+                    Span::styled(chr.to_string(), focused_code_style)
+                } else if program.instruction_positions.contains(&(i, j)) {
+                    Span::styled(chr.to_string(), code_style)
+                } else {
+                    Span::styled(chr.to_string(), comment_style)
+                }
+            }))
+            .collect::<Vec<_>>();
 
             Spans::from(spans)
         })
